57982dfa29f83d1958603fdae9cd4bb2b36cc27a
Max Antinone <mantinone@gmail.com>
2017-01-02 13:36:59 -0800

    Webpages that access the database

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var hardCoded = require('./routes/hardCoded')
+var dbaccess = require('./routes/dbaccess')
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/hardCoded', hardCoded);
+app.use('/dbaccess', dbaccess);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 //This route is 'localhost:300/', or just 'localhost:300'
 router.get('/', function(req, res, next) {
   //The first argument below tells the us which html(or pug in our case) file to load.  The second argument is an object that is sent to that page.
-  res.render('index', { title: 'Tutorial Site' });
-});
+  res.render('index', { title: 'Tutorial Site' })
+})
 
 module.exports = router;
